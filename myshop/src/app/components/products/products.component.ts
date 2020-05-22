d2676e553df232911df5ae2b5e004ca68703435a
ruta96 <ruciak1996@gmail.com>
2020-05-22 21:38:46 +0200

    Laboratorium 4

--- myshop/src/app/components/products/products.component.ts
+++ myshop/src/app/components/products/products.component.ts
@@ -15,5 +15,10 @@ export class ProductsComponent implements OnInit {
   getProducts(){
     this.mainService.getProducts(this.mainService.productsRequest);
   }
+  addToCart(product) {
+    this.mainService.cart.push({ product });
+    console.log('dodano do koszyka produkt:');
+    console.log(product);
+  }
 
 }
